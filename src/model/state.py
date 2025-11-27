246a961f4dacf9dda163ff217b41e9078a10789f
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-11-27 00:45:55 -0300

    fazendo mudanças

--- src/model/state.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True, slots=True)
-class AppState:
-    """Estado imutável da aplicação (apenas valor do slider para o MVP)."""
-    slider_value: float
